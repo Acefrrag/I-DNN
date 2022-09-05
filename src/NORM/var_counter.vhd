4529c9747a33552e04098d04ecbf1a4e4485d637
Acefrrag <59066474+Acefrrag@users.noreply.github.com>
2022-09-06 00:01:01 +0200

    Started extensive commenting of code. Description section of entities, line commenting, entity's port and parameters commenting

--- src/NORM/var_counter.vhd
+++ src/NORM/var_counter.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: University of Trento
+-- Engineer: Simone Ruffini
 -- 
 -- Create Date: 06/26/2020 05:19:23 PM
 -- Design Name: 
@@ -8,13 +8,16 @@
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
--- Description: 
+-- Description: This entity is used to fetch elements to recover from and save into the non-volatile register. It is syncronized with the
+-- var_cntr_clk which depends on the intrinsic speed of the non-volatile register.
+-- 
 -- 
 -- Dependencies: 
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments:
+-- Revision 0.02 Michele Pio Fragasso - Wrote Description
+-- Additional Comments: 
 -- 
 ----------------------------------------------------------------------------------
 
@@ -22,30 +25,21 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity var_counter is
     Generic(
-        MAX         : INTEGER;
-        INIT_VALUE  : INTEGER;
-        INCREASE_BY : INTEGER
+        MAX         : INTEGER;          --MAX           : unused. It contains the upper limit of the counter
+        INIT_VALUE  : INTEGER;          --INIT_VALUE    : This parameter specifies where the counter starts from when the counter is initialized
+        INCREASE_BY : INTEGER           --INCREASE_BY   : This paramter specifies the increment at every clk cycle
     );
     Port (
         
-        clk         : in STD_LOGIC;
-        resetn      : in STD_LOGIC;
-        INIT        : in STD_LOGIC;
-        CE          : in STD_LOGIC;
-        end_value   : in INTEGER RANGE 0 TO MAX;
-        TC          : out STD_LOGIC;
-        value       : out INTEGER RANGE 0 TO MAX
+        clk         : in STD_LOGIC;                     --clk       :clock signal
+        resetn      : in STD_LOGIC;                     --resetn    :active on low reset signal
+        INIT        : in STD_LOGIC;                     --INIT      :Init bit to initialize the var_counter_value with INIT_VALUE
+        CE          : in STD_LOGIC;                     --CE        :Counter enable bit. '0': Disabled '1': Enabled
+        end_value   : in INTEGER RANGE 0 TO MAX;        --end_value :Signal containing the upper counter limit. When counter value passes this value, the counter is reset.
+        TC          : out STD_LOGIC;                    --TC        :Counter Terminal Bit. When counter value reaches end value this is set to '1' aknowledging the reach of the last value 
+        value       : out INTEGER RANGE 0 TO MAX        --value     .Counter Value. It is increment by INCREASE_BY at every cycle of clk.
     );
 end var_counter;
 
